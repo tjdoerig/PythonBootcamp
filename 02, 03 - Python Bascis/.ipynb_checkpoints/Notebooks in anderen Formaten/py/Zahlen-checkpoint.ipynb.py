b44ac161da9404e08c3d88d21cfadaca0284f5bb
tjdoerig <doerig.thomas@gmail.com>
2021-01-16 23:09:54 +0100

    start

---
+++ 02, 03 - Python Bascis/.ipynb_checkpoints/Notebooks in anderen Formaten/py/Zahlen-checkpoint.ipynb.py
@@ -0,0 +1,70 @@
+# Diese .py - Datei wurde automatisch aus dem IPython - Notebook (.ipynb) generiert.
+# 
+# Gelegentlich wurde ich von Teilnehmern gefragt, ob ich die Kursmaterialien nicht 
+# auch als normale .py - Datien bereitstellen könne. Dadurch ist es möglich, den Code
+# ohne Jupyter zu öffnen, beispielsweise wenn Python-Programme in einem Terminal oder in 
+# Eclipse entwickelt werden.
+# 
+# Dem möchte ich hiermit nachkommen. Ich empfehle dir aber trotzdem, schau' dir lieber die
+# IPython - Notebooks direkt an, oder den HTML-Export eben dieser. Dieser reine .py-Export
+# ist meiner Meinung nach etwas unübersichtlich.
+# 
+# Bitte beachte zudem, dass du Pfadangaben ggf. manuell korrigieren musst!
+# 
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Zahlen in Python
+# 
+# Zahlen funktionieren in Python ähnlich wie sonst überall auch - du kannst damit ganz normale Rechnungen berechnen.
+
+# In[8]:
+
+
+print(5)
+
+
+# In[9]:
+
+
+print(5)
+print(6)
+
+
+# ### Die Grundrechenarten
+
+# In[10]:
+
+
+print(5 + 4)
+print(5 - 4)
+print(5 * 4)
+print(5 / 4)
+
+
+# Auch die Klammersetzung funktioniert natürlich wie gewohnt :-):
+
+# In[11]:
+
+
+print((5 + 4) * 3)
+
+
+# In[15]:
+
+
+print(1.25 + 2)
+
+
+# In[14]:
+
+
+print(1 + 2)
+
+
+# In[ ]:
+
+
+
+
+
